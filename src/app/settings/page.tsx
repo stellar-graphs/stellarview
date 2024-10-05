d72d09513df041a9ed3f4b3a2667357f63558ae4
Philippe Höij <philippe@hoij.net>
2024-10-05 11:59:30 +0200

    added top bar

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title: "Stellar Graph Settings",
+  description:
+    "Edit your settings",
+};
+
+const Settings = () => {
+  return (
+    <DefaultLayout>
+      <></>
+    </DefaultLayout>
+  );
+};
+
+export default Settings;
---
+++ src/components/Layouts/DefaultLayout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState, ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
+import Header from "./Header";
+
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false); // Switch to Redux state
+
+  return (
+    <>
+      <div className="flex">
+        {/* <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
+
+        <div className="relative flex flex-1 flex-col lg:ml-72.5">
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+              {sidebarOpen ? <>open</> : <>closed</>}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
